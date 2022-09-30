749435335e1f8bce2c551bb1328f72c4b95b23d4
Temiloluwa Awoyele <awoyeletemiloluwa@gmail.com>
2022-09-30 15:40:35 +0100

    fix: randomize uuid with file change

--- terraform/data_sources.tf
+++ terraform/data_sources.tf
@@ -1,12 +1,8 @@
 data "aws_caller_identity" "account_details" {}
 
-resource "random_uuid" "output_uuid" {
-
-}
-
 data "archive_file" "zip_python_code" {
   type             = "zip"
-  source_file      = "${path.module}/../lambda/index.py"
+  source_file      = local.lambda_source_file
   output_path      = "${path.module}/../files/output-${random_uuid.output_uuid.result}.zip"
   output_file_mode = "0666"
 }
--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,7 @@
+locals {
+  lambda_source_file = "${path.module}/../lambda/index.py"
+}
+
 resource "aws_s3_bucket" "bucket" {
   bucket = "${var.bucket_prefix}-${data.aws_caller_identity.account_details.account_id}"
   tags = {
@@ -29,9 +33,18 @@ resource "aws_lambda_function" "nyc_lambda_function" {
   runtime       = "python3.9"
   filename      = data.archive_file.zip_python_code.output_path
   handler       = "index.lambda_handler"
+  depends_on = [
+    random_uuid.output_uuid
+  ]
 }
 
 resource "aws_iam_role_policy_attachment" "lambda_cloudwatch_attach_policy" {
   role       = aws_iam_role.lambda_role.id
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
+resource "random_uuid" "output_uuid" {
+  keepers = {
+    a = filemd5(local.lambda_source_file)
+  }
 }
