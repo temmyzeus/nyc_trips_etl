945e38a73dbe324419a59a90c99412270e00a7f5
Temiloluwa Awoyele <awoyeletemiloluwa@gmail.com>
2022-09-30 12:24:06 +0100

    Test: print events and context

--- lambda/index.py
+++ lambda/index.py
@@ -2,6 +2,10 @@ import json
 
 def lambda_handler(event, context):
     # TODO implement
+
+    print("Event:", event, end="\n\n")
+    print("Context:", context)
+    
     return {
         'statusCode': 200,
         'body': "Great"
