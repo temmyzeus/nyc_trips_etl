767966b7cc9be6795445cb3ca5c31c1c63b04806
Temiloluwa Awoyele <awoyeletemiloluwa@gmail.com>
2022-10-07 10:09:29 +0000

    Create download url dag

--- dags/nyc-trips.py
+++ dags/nyc-trips.py
@@ -1,5 +1,6 @@
 import os
-from datetime import datetime
+from datetime import datetime, date
+from typing import Union
 
 from airflow.decorators import dag, task
 from airflow.operators.dummy import DummyOperator
@@ -12,6 +13,52 @@ from utils import checks
 
 LAMBDA_FUNCTION_NAME:str = os.getenv("NYC_LAMBDA")
 BUCKET_NAME:str = os.getenv("S3_BUCKET_NAME")
+
+date_month_type = Union[int, str]
+
+def create_download_url(ti, year: date_month_type, month: date_month_type) -> None:
+    """
+    Create data download link based on date and availability of the data and save as xcoms.
+
+    Yellow Trips records: Jan 2009 - till
+    Green Trips records: August 2013 - till
+    For-Hire Vehicle Trip Records: January 2015 - till
+    High Volume For-Hire Vehicle Trip Records: February 2019 - till
+    """
+
+    if isinstance(year, str):
+        year = int(year)
+
+    if isinstance(month, str):
+        month = int(month)
+
+    run_interval_date:date = date(year, month, 1)
+
+    ti.xcom_push(
+        key="yellow_trips_url", 
+        value="https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_{}-{:02d}.parquet".format(year, month)
+        )
+
+    if run_interval_date > date(2013, 8, 1):
+        ti.xcom_push(
+            key="green_trips_url",
+            value="https://d37ci6vzurychx.cloudfront.net/trip-data/green_tripdata_{}-{:02d}.parquet".format(year, month)
+        )
+        print("Green Trips now Available")
+
+    if run_interval_date > date(2015, 1, 1):
+        ti.xcom_push(
+            key="fhv_trips_url",
+            value="https://d37ci6vzurychx.cloudfront.net/trip-data/fhv_tripdata_{}-{:02d}.parquet".format(year, month)
+        )
+        print("For-Hire Vehicle now available")
+
+    if run_interval_date > date(2019, 2, 1):
+        ti.xcom_push(
+            key="high_volume_fhv_url",
+            value="https://d37ci6vzurychx.cloudfront.net/trip-data/fhv_tripdata_{}-{:02d}.parquet".format(year, month)
+        )
+        print("High Volume For-Hire Vehicle Trip available")
 
 @dag(
     dag_id="NYC-Trips",
@@ -41,8 +88,13 @@ def dag():
         op_kwargs={"function_name": LAMBDA_FUNCTION_NAME}
     )
 
-    fetch_data_to_s3_with_lambda = DummyOperator(
-        task_id="fetch_data_to_s3_with_lambda"
+    create_download_links = PythonOperator(
+        task_id="create_download_links",
+        python_callable=create_download_url,
+        op_kwargs={
+            "year": "{{ dag_run.logical_date.year }}",
+            "month": "{{ dag_run.logical_date.month }}",
+        }
     )
 
     # download_to_s3 = AwsLambdaInvokeFunctionOperator(
